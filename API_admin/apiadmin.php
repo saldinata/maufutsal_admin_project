7c9ab3d1cbb51fcf80e91a42a172011e5b7de2ae
DESKTOP-NUJODEV\saldin <saldinata.ardani@gmail.com>
2017-05-31 00:38:12 +0700

    - 31 May 2017,  12:38 ( Migration to Netbeans IDE) -

--- API_admin/apiadmin.php
+++ API_admin/apiadmin.php
@@ -1,125 +1,122 @@
 <?php
-  require_once("../libs/act.class.php");
-  require_once("../libs/auth.class.php");
-  require_once("../libs/database.class.php");
-  require_once("../libs/utility.class.php");
-  require_once("../libs/class.smtp.php");
-  require_once("../libs/phpmailer.php");
 
-  $db   = new Database();
-  $util = new Utility();
-  $auth = new Authentication($db,$util);
-  $act  = new ActivityApps($db,$util,$auth);
-  $mail = new PHPMailer();
+require_once("../libs/act.class.php");
+require_once("../libs/auth.class.php");
+require_once("../libs/database.class.php");
+require_once("../libs/utility.class.php");
+require_once("../libs/class.smtp.php");
+require_once("../libs/phpmailer.php");
 
-  if(isset($_POST['type']) && !empty($_POST['type']))
-  {
-    switch($_POST['type'])
-    {
-      case 'reqstoredistdat':
-         $act->reqstoredistdat($_POST['dist_name'],$_POST['dist_addr'],$_POST['dist_pic'],$_POST['dist_private_phone'],$_POST['dist_office_phone'],$_POST['dist_mail']);
-      break;
+$db = new Database();
+$util = new Utility();
+$auth = new Authentication($db, $util);
+$act = new ActivityApps($db, $util, $auth);
+$mail = new PHPMailer();
 
-      case 'reqstoredistpro':
-         $act->reqstoredistpro($_POST['dist_name'],$_POST['dist_product'],$_POST['dist_price'],$_POST['dist_note']);
-      break;
+if (isset($_POST['type']) && !empty($_POST['type'])) {
+    switch ($_POST['type']) {
+        case 'reqstoredistdat':
+            $act->reqstoredistdat($_POST['dist_name'], $_POST['dist_addr'], $_POST['dist_pic'], $_POST['dist_private_phone'], $_POST['dist_office_phone'], $_POST['dist_mail']);
+            break;
 
-      case 'reqgetalldistpro':
-         $act->reqgetalldistpro();
-      break;
+        case 'reqstoredistpro':
+            $act->reqstoredistpro($_POST['dist_name'], $_POST['dist_product'], $_POST['dist_price'], $_POST['dist_note']);
+            break;
 
-      case 'reqorderdistpro':
-         $act->reqorderdistpro($_POST['futsal_code'],$_POST['nominal'],$_POST['id_product']);
-      break;
+        case 'reqgetalldistpro':
+            $act->reqgetalldistpro();
+            break;
 
-      case 'reqchklogin':
-         $auth->reqchklogin($_POST['user'],$_POST['pass']);
-      break;
+        case 'reqorderdistpro':
+            $act->reqorderdistpro($_POST['futsal_code'], $_POST['nominal'], $_POST['id_product']);
+            break;
 
-      case 'reqsaveaboutcontent':
-         $act->reqsaveaboutcontent($_POST['contents']);
-      break;
+        case 'reqchklogin':
+            $auth->reqchklogin($_POST['user'], $_POST['pass']);
+            break;
 
-      case 'reqsaveprivacycontent':
-         $act->reqsaveprivacycontent($_POST['contents']);
-      break;
+        case 'reqsaveaboutcontent':
+            $act->reqsaveaboutcontent($_POST['contents']);
+            break;
 
-      case 'reqsavecareercontent':
-         $act->reqsavecareercontent($_POST['contents']);
-      break;
+        case 'reqsaveprivacycontent':
+            $act->reqsaveprivacycontent($_POST['contents']);
+            break;
 
-      case 'reqsavecontactcontent':
-         $act->reqsavecontactcontent($_POST['phone'],$_POST['address'],$_POST['company'],$_POST['email']);
-      break;
+        case 'reqsavecareercontent':
+            $act->reqsavecareercontent($_POST['contents']);
+            break;
 
-      case 'reqsavesocialcontent':
-         $act->reqsavesocialcontent($_POST['facebook'],$_POST['twitter'],$_POST['instagram'],$_POST['google']);
-      break;
+        case 'reqsavecontactcontent':
+            $act->reqsavecontactcontent($_POST['phone'], $_POST['address'], $_POST['company'], $_POST['email']);
+            break;
 
-      case 'reqpubimg':
-         $act->reqpubimg($_POST['id']);
-      break;
+        case 'reqsavesocialcontent':
+            $act->reqsavesocialcontent($_POST['facebook'], $_POST['twitter'], $_POST['instagram'], $_POST['google']);
+            break;
 
-      case 'reqnopubimg':
-         $act->reqnopubimg($_POST['id']);
-      break;
+        case 'reqpubimg':
+            $act->reqpubimg($_POST['id']);
+            break;
 
-      case 'reqdelsliderimg':
-         $act->reqdelsliderimg($_POST['id']);
-      break;
+        case 'reqnopubimg':
+            $act->reqnopubimg($_POST['id']);
+            break;
 
-      case 'reqdelmemacc':
-         $act->reqdelmemacc($_POST['id']);
-      break;
+        case 'reqdelsliderimg':
+            $act->reqdelsliderimg($_POST['id']);
+            break;
 
-      case 'reqdelownacc':
-         $act->reqdelownacc($_POST['id']);
-      break;
+        case 'reqdelmemacc':
+            $act->reqdelmemacc($_POST['id']);
+            break;
 
-      case 'reqresetowneracc':
-         $act->reqresetowneracc($_POST['id']);
-      break;
+        case 'reqdelownacc':
+            $act->reqdelownacc($_POST['id']);
+            break;
 
-      case 'reqadduser':
-         $auth->reqadduser($_POST['name'],$_POST['username'],$_POST['password'],$_POST['level_user']);
-      break;
+        case 'reqresetowneracc':
+            $act->reqresetowneracc($_POST['id']);
+            break;
 
-      case 'reqconfieresev':
-         $act->reqconfieresev($_POST['idbooking']);
-      break;
+        case 'reqadduser':
+            $auth->reqadduser($_POST['name'], $_POST['username'], $_POST['password'], $_POST['level_user']);
+            break;
 
-      case 'reqpayresfiedet':
-         $act->reqpayresfiedet($_POST['idbooking']);
-      break;
+        case 'reqconfieresev':
+            $act->reqconfieresev($_POST['idbooking']);
+            break;
 
-      case 'reqconfigpayturn':
-         $act->reqconfigpayturn($_POST['idbooking']);
-      break;
+        case 'reqpayresfiedet':
+            $act->reqpayresfiedet($_POST['idbooking']);
+            break;
 
-      case 'reqpayturninfo':
-        $act->reqpayturninfo($_POST['idbooking']);
-      break;
+        case 'reqconfigpayturn':
+            $act->reqconfigpayturn($_POST['idbooking']);
+            break;
 
-      case 'reqlogout':
-         $auth->reqlogout();
-      break;
+        case 'reqpayturninfo':
+            $act->reqpayturninfo($_POST['idbooking']);
+            break;
 
-      case 'reqapprate':
-         $act->reqapprate($_POST['idharga']);
-      break;
+        case 'reqlogout':
+            $auth->reqlogout();
+            break;
 
-      case 'reqrejectrate':
-         $act->reqrejectrate($_POST['idharga']);
-      break;
-      
-      case 'reqchangestate' :
-          $auth->reqchangestate($_POST['iduser']);
-      break;
-  
-      default:
-      break;
+        case 'reqapprate':
+            $act->reqapprate($_POST['idharga']);
+            break;
+
+        case 'reqrejectrate':
+            $act->reqrejectrate($_POST['idharga']);
+            break;
+
+        case 'reqchangestate' :
+            $auth->reqchangestate($_POST['iduser']);
+            break;
+
+        default:
+            break;
     }
-  }
-
-
- ?>
+}
+?>
--- API_admin/uploadslider.php
+++ API_admin/uploadslider.php
@@ -3,60 +3,47 @@
 require_once('../libs/database.class.php');
 require_once('../libs/utility.class.php');
 
-$db 	= new Database();
+$db = new Database();
 $util = new Utility();
 
-if(isset($_FILES["file"]["type"]))
-{
-	$validextensions = array("jpeg", "jpg", "png");
-	$temporary = explode(".", $_FILES["file"]["name"]);
-	$file_extension = end($temporary);
+if (isset($_FILES["file"]["type"])) {
+    $validextensions = array("jpeg", "jpg", "png");
+    $temporary = explode(".", $_FILES["file"]["name"]);
+    $file_extension = end($temporary);
 
-	if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")) && ($_FILES["file"]["size"] < 9000000) && in_array($file_extension, $validextensions))
-	{
-		if ($_FILES["file"]["error"] > 0)
-		{
-			echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
-		}
-		else
-		{
-			if (file_exists("../image/slider/" . $_FILES["file"]["name"]))
-			{
-				echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
-			}
-			else
-			{
-				$default_state	= "NA";
-				$sourcePath 	= trim($_FILES['file']['tmp_name']);
-				$targetPath 	= "../image/slider/".trim($_FILES['file']['name']);
-				//$targetPathURL = "www.maufutsal.com/newadmin/image/slider/".$_FILES['file']['name'];
-				$targetPathURL = "../image/slider/".trim($_FILES['file']['name']);
+    if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")) && ($_FILES["file"]["size"] < 9000000) && in_array($file_extension, $validextensions)) {
+        if ($_FILES["file"]["error"] > 0) {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
+        } else {
+            if (file_exists("../image/slider/" . $_FILES["file"]["name"])) {
+                echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
+            } else {
+                $default_state = "NA";
+                $sourcePath = trim($_FILES['file']['tmp_name']);
+                $targetPath = "../image/slider/" . trim($_FILES['file']['name']);
+                //$targetPathURL = "www.maufutsal.com/newadmin/image/slider/".$_FILES['file']['name'];
+                $targetPathURL = "../image/slider/" . trim($_FILES['file']['name']);
 
-				move_uploaded_file($sourcePath,$targetPath) ;
+                move_uploaded_file($sourcePath, $targetPath);
 
-				// echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
-				// echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
-				// echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
-				// echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-				// echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+                // echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
+                // echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
+                // echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
+                // echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+                // echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
 
-				$query="INSERT INTO tbl_slider(path, state) VALUES(?,?)";
-				$result_insert_pict = $db->insertValue($query,[$_FILES["file"]["name"],$default_state]);
+                $query = "INSERT INTO tbl_slider(path, state) VALUES(?,?)";
+                $result_insert_pict = $db->insertValue($query, [$_FILES["file"]["name"], $default_state]);
 
-				if($result_insert_pict)
-				{
-					echo "success";
-				}
-				else
-				{
-					echo "gagal";
-				}
-			}
-		}
-	}
-	else
-	{
-		echo "<span id='invalid'>***Invalid file Size or Type***<span>";
-	}
+                if ($result_insert_pict) {
+                    echo "success";
+                } else {
+                    echo "gagal";
+                }
+            }
+        }
+    } else {
+        echo "<span id='invalid'>***Invalid file Size or Type***<span>";
+    }
 }
 ?>
