d01e953361d450814e5140e2b97343c481cf3ecf
DESKTOP-NUJODEV\saldin <saldinata.ardani@gmail.com>
2017-06-12 11:40:24 +0700

    - 12 Juni 2017, 11:40 .a.m -

--- pages/list_field.php
+++ pages/list_field.php
@@ -1,69 +1,74 @@
 <div class="content">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <div class="page-title-box">
-            <h4 class="page-title">Daftar Lapangan Futsal</h4>
-            <div class="clearfix"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="page-title-box">
+                    <h4 class="page-title">Daftar Lapangan Futsal</h4>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="card-box table-responsive">
-          <h4 class="m-t-0 header-title" style="font-weight: 300;">
-            Lapangan Futsal
-          </h4>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card-box table-responsive">
+                    <h4 class="m-t-0 header-title" style="font-weight: 300;">
+                        Lapangan Futsal
+                    </h4>
 
-            <hr style="border-top: 1px solid #e5e4e4;"/>
+                    <hr style="border-top: 1px solid #e5e4e4;"/>
 
-            <table id="datatable" class="table table-striped table-colored table-info">
-              <thead>
-                <tr>
-                    <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Nama Lapangan</th>
-                    <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Alamat</th>
-                    <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Kota</th>
-                    <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Provinsi</th>
-                    <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Tanggal Bergabung</th>
+                    <table id="datatable" class="table table-striped table-colored table-info">
+                        <thead>
+                            <tr>
+                                <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Nama Lapangan</th>
+                                <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Alamat</th>
+                                <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Kota</th>
+                                <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Provinsi</th>
+                                <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">Tanggal Bergabung</th>
+                                <th style="background-color: #656664;font-size: 13px;text-align: center;font-size: 11px;">User Kepemilikan</th>
+                            </tr>
+                        </thead>
 
+                        <tbody>
+                            <?php
+                            $query          = "SELECT * FROM tbl_field_information";
+                            $get_field_info = $db->getAllValue($query);
 
-                </tr>
-              </thead>
+                            foreach ($get_field_info as $data_info)
+                            {
+                                $date_join = $util->changeFormatDateFromNumberToString(substr($data_info['court_reg'],
+                                                0, 8));
 
-              <tbody>
-                <?php
-                  $query = "SELECT * FROM tbl_field_information";
-                  $get_field_info = $db->getAllValue($query);
+                                $id_prov = $data_info['id_prov'];
+                                $id_city = $data_info['id_kota'];
+                                $id_user = $data_info['id_user'];
 
-                  foreach($get_field_info as $data_info)
-                  {
-                     $date_join = $util->changeFormatDateFromNumberToString(substr($data_info['court_reg'],0,8));
+                                $query       = "SELECT * FROM tbl_user WHERE id_user=?";
+                                $result_user = $db->getValue($query, [$id_user]);
 
-                     $id_prov = $data_info['id_prov'];
-                     $id_city = $data_info['id_kota'];
+                                $query     = "SELECT * FROM tbl_provinsi WHERE provinsi_id=?";
+                                $prov_name = $db->getValue($query, [$id_prov]);
 
-                     $query   = "SELECT * FROM tbl_provinsi WHERE provinsi_id=?";
-                     $prov_name  = $db->getValue($query,[$id_prov]);
+                                $query     = "SELECT * FROM tbl_city WHERE kota_id=?";
+                                $city_name = $db->getValue($query, [$id_city]);
 
-                     $query   = "SELECT * FROM tbl_city WHERE kota_id=?";
-                     $city_name  = $db->getValue($query,[$id_city]);
-
-                     echo "<tr>";
-                     echo "<td style=\"font-size: 11px;\">".$data_info['nama_lapangan']."</td>";
-                     echo "<td style=\"font-size: 11px;\">".$data_info['alamat']."</td>";
-                     echo "<td class=\"text-center\" style=\"font-size: 11px; \">".$city_name['kokab_nama']."</td>";
-                     echo "<td class=\"text-center\" style=\"font-size: 11px;\">".$prov_name['provinsi_nama']."</td>";
-                     echo "<td class=\"text-center\" style=\"font-size: 11px;\">".$date_join."</td>";
-                     echo "</tr>";
-                  }
-                 ?>
-              </tbody>
-            </thead>
-          </table>
+                                echo "<tr>";
+                                echo "<td style=\"font-size: 11px;\">" . $data_info['nama_lapangan'] . "</td>";
+                                echo "<td style=\"font-size: 11px;\">" . $data_info['alamat'] . "</td>";
+                                echo "<td class=\"text-center\" style=\"font-size: 11px; \">" . $city_name['kokab_nama'] . "</td>";
+                                echo "<td class=\"text-center\" style=\"font-size: 11px;\">" . $prov_name['provinsi_nama'] . "</td>";
+                                echo "<td class=\"text-center\" style=\"font-size: 11px;\">" . $date_join . "</td>";
+                                echo "<td class=\"text-center\" style=\"font-size: 11px;\">" . $result_user['username'] . "</td>";
+                                echo "</tr>";
+                            }
+                            ?>
+                        </tbody>
+                        </thead>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
 
 
